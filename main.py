afba982db0bf453f3ea2aae5d2fd72a8bb0f7f8a
jkoenigschulte <beaconplace@web.de>
2025-11-20 16:17:27 +0100

    what the helly

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from XFLRextraction import extract_main_wing_data
 from Interpolation import interpolate
+from Linearmodel import linear_model
 import matplotlib.pyplot as plt
 import numpy as np
 
-ylst,cllst,cdlst,cmlst = extract_main_wing_data("MainWing_a=10.00_v=10.00ms.txt")
-fcl,fcd,fcm = interpolate(ylst,cllst,cdlst,cmlst)
+ylst,cllst,cdlst,cmlst,CL10 = extract_main_wing_data("MainWing_a=10.00_v=10.00ms.txt")
+fcl10,fcd10,fcm10 = interpolate(ylst,cllst,cdlst,cmlst)
+ylst,cllst,cdlst,cmlst,CL0 = extract_main_wing_data("MainWing_a=0.00_v=10.00ms.txt")
+fcl0,fcd0,fcm0 = interpolate(ylst,cllst,cdlst,cmlst)
+
+Cl = linear_model(fcl0,fcl10,CL10,CL0)
 
 
 
-
-
+"""
 # Example usage of the interpolated functions
 ylst = np.linspace(0, max(ylst), 100)
 
@@ -20,3 +24,4 @@ plt.xlabel("x")
 plt.ylabel("f(x)")
 plt.grid(True)
 plt.show()
+"""
