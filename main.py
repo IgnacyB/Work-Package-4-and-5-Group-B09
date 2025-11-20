a03b118aa63b707ef82ab5e7a8d5678ab3ac2263
Jan Horawa <jan.horawa@gmail.com>
2025-11-20 16:35:20 +0100

    Added plotting for Cl(y,CL)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from Interpolation import interpolate
 from Linearmodel import linear_model
 import matplotlib.pyplot as plt
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 
 ylst,cllst,cdlst,cmlst,CL10 = extract_main_wing_data("MainWing_a=10.00_v=10.00ms.txt")
 fcl10,fcd10,fcm10 = interpolate(ylst,cllst,cdlst,cmlst)
@@ -12,6 +13,29 @@ fcl0,fcd0,fcm0 = interpolate(ylst,cllst,cdlst,cmlst)
 Cl = linear_model(fcl0,fcl10,CL10,CL0)
 Cd = linear_model(fcd0,fcd10,CL10,CL0)
 Cm = linear_model(fcm0,fcm10,CL10,CL0)
+
+# Create ranges
+y_vals  = np.linspace(0, max(ylst), 200)
+CL_vals = np.linspace(CL0, CL10, 200)
+
+# Create grid
+Y, CLgrid = np.meshgrid(y_vals, CL_vals)
+
+# Evaluate your function on the grid
+Z = Cl(Y, CLgrid)
+
+# Plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+surf = ax.plot_surface(Y, CLgrid, Z)
+
+ax.set_xlabel("y")
+ax.set_ylabel("CL")
+ax.set_zlabel("Cl(y, CL)")
+
+plt.show()
+
 
 
 """
