2cb3006341923e886f0456feef47e1989e0e23c2
lisanne-b <l.m.l.boer@student.tudelft.nl>
2025-11-27 14:55:02 +0100

    graphs

--- Deflection_Graphs.py
+++ Deflection_Graphs.py
@@ -1,18 +1,35 @@
+import matplotlib.pyplot as plt
+
 from Wing_geometry import b
 from Twist_Distribution import twist
 # from Lateral_Deflection import
 
-y_position = 0
-spanpos = []
-twist_dist = []
-lateral_dist = []
 
 #lateral deflection graph
+def lateral_deflection_graph():
+    spanpos = []
+    lateral_dist = []
+
+    y_position = 0
+
+    while y_position <= b/2:
+        spanpos.append(y_position)
+        lateral_dist.append()
+        y_position += 0.5
 
 #twist distribution
+def twist_distribution_graph():
+    spanpos = []
+    twist_dist = []
 
-while y_position <= b/2:
-    twist_dist.append(twist(y_position))
-    y += 0.5
+    y_position = 0
+    
+    while y_position <= b/2:
+        spanpos.append(y_position)
+        twist_dist.append(twist(y_position))
+        y_position += 0.5
 
-
+    plt.plot(spanpos, twist_dist)
+    plt.xlabel("Spanwise position [m]")
+    plt.ylabel("Twist distribution [UNIT]")
+    plt.show
