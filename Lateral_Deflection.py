0039be49ec72273171b5bc92eb1aa65abbb0943f
lisanne-b <l.m.l.boer@student.tudelft.nl>
2025-11-27 17:31:27 +0100

    tried to fix Lateral_Deflection

--- Lateral_Deflection.py
+++ Lateral_Deflection.py
@@ -3,11 +3,13 @@
 #first import the necessary packages and functions from 
 import numpy as np
 import scipy as sp
+from scipy.integrate import cumtrapz
 
 #import all the information from the other files
 from material_properties import E
 from MOI import MOI_single_cell
 from load_calculations import M
+
 
 def h(y):
     return M(y) / (E * MOI_single_cell(y))
@@ -15,7 +17,7 @@ def h(y):
 print(h(5))
 
 def dvdy(y):
-    return -1 * sp.integrate.quad(h,0,y)[0]
+    return -1 * sp.integrate.quad(np.vectorize(h),0,y)[0]
 
 print(dvdy(5))
 
@@ -24,4 +26,37 @@ def lateral_deflection(y):
     return sp.integrate.quad(dvdy,0,y)[0]
 
 print(lateral_deflection(0.1))
+
+# import numpy as np
+# from scipy.integrate import cumulative_trapezoid as cumtrapz
+
+# from material_properties import E
+# from Aircraft_parameters import b
+# from MOI import MOI_single_cell
+# from load_calculations import M
+
+# def h(y):
+#     return M(y) / (E * MOI_single_cell(y))
+
+# L = b/2
+# N = 10
+# ys = np.linspace(0, L, N)
+
+# # Vectorize h
+# h_vec = np.vectorize(h)
+# h_vals = h_vec(ys)
+
+# # first integration
+# dvdy_vals = -cumtrapz(h_vals, ys, initial=0)
+
+# # second integration
+# v_vals = cumtrapz(dvdy_vals, ys, initial=0)
+
+# def dvdy(y):
+#     return np.interp(y, ys, dvdy_vals)
+
+# def lateral_deflection(y):
+#     return np.interp(y, ys, v_vals)
+
+# print(lateral_deflection(0.1))
 
