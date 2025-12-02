c2e7344ae271b49447b5ab212bcf18074d64e054
zorkafischer <zorka@dokiten.io>
2025-12-02 15:31:07 +0100

    summarised values

--- Obtained_Values_Summary.py
+++ Obtained_Values_Summary.py
@@ -1,12 +1,16 @@
 import math
 
-from MOI import value, MOI_single_cell
-#from CENTROID import cx,cy
+from MOI import value
+from
+from CENTROID import get_centroid
 from Lateral_Deflection import lateral_deflection_at_tip
 from Twist_Distribution import twist_at_tip
+from airfoil_geometry import t_front as thickness
+from airfoil_geometry import a_stringer as mass_stringer
+from airfoil_geometry import n_stringer
 ## Centroid ##
-
-#print(cx,cy)
+#chord_at_tip = c_r-((c_r-c_t)/(b/2))*(b/2)
+#x_centroid, y,centroid = get_centroid(chord_at_tip,spars, thickness,mass_stringer,n_stringer)
 
 ## Moment of Inertia ##
 
