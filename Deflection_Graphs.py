84714ae0bc6a8b087c3cf5d0d00d7ad5c192e225
lisanne-b <l.m.l.boer@student.tudelft.nl>
2025-12-02 15:15:06 +0100

    fixed graphs

--- Deflection_Graphs.py
+++ Deflection_Graphs.py
@@ -6,22 +6,23 @@ from Twist_Distribution import twist_function
 from Lateral_Deflection import lateral_deflection_function
 
 
-# #lateral deflection graph
-# def plot_lateral_deflection():
-#     spanpos = []
-#     lateral_dist = []
+#lateral deflection graph
+def plot_lateral_deflection():
+    spanpos = []
+    lateral_dist = []
 
-#     y_position = 0
+    y_position = 0
 
-#     while y_position <= b/2:
-#         spanpos.append(y_position)
-#         lateral_dist.append(lateral_deflection(y_position))
-#         y_position += 0.5
+    while y_position <= b/2:
+        spanpos.append(y_position)
+        lateral_dist.append(lateral_deflection_function(y_position))
+        y_position += 0.5
+        # print(y_position)
 
-#     plt.plot(spanpos, lateral_dist)
-#     plt.xlabel("Spanwise position [m]")
-#     plt.ylabel("Lateral deflection [m]")
-#     plt.show
+    plt.plot(spanpos, lateral_dist)
+    plt.xlabel("Spanwise position [m]")
+    plt.ylabel("Lateral deflection [m]")
+    plt.show()
 
 
 #twist distribution
@@ -35,7 +36,7 @@ def plot_twist_distribution():
         spanpos.append(y_position)
         twist_dist.append(twist_function(y_position)*180/math.pi)
         y_position += 0.5
-        print(y_position)
+        # print(y_position)
 
     plt.plot(spanpos, twist_dist)
     plt.xlabel("Spanwise position [m]")
