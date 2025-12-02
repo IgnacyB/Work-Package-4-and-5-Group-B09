1da31e223f710c7dd5ac51dcf6d0c570c26f2282
lisanne-b <l.m.l.boer@student.tudelft.nl>
2025-12-02 14:34:01 +0100

    graphs

--- Deflection_Graphs.py
+++ Deflection_Graphs.py
@@ -2,30 +2,30 @@ import matplotlib.pyplot as plt
 import math
 
 from Aircraft_parameters import b
-from Twist_Distribution import twist
-from Lateral_Deflection import lateral_deflection
+from Twist_Distribution import twist_function
+from Lateral_Deflection import lateral_deflection_function
 
 
-#lateral deflection graph
-def lateral_deflection_graph():
-    spanpos = []
-    lateral_dist = []
+# #lateral deflection graph
+# def plot_lateral_deflection():
+#     spanpos = []
+#     lateral_dist = []
 
-    y_position = 0
+#     y_position = 0
 
-    while y_position <= b/2:
-        spanpos.append(y_position)
-        lateral_dist.append(lateral_deflection(y_position))
-        y_position += 0.5
+#     while y_position <= b/2:
+#         spanpos.append(y_position)
+#         lateral_dist.append(lateral_deflection(y_position))
+#         y_position += 0.5
 
-    plt.plot(spanpos, lateral_dist)
-    plt.xlabel("Spanwise position [m]")
-    plt.ylabel("Lateral deflection [m]")
-    plt.show
+#     plt.plot(spanpos, lateral_dist)
+#     plt.xlabel("Spanwise position [m]")
+#     plt.ylabel("Lateral deflection [m]")
+#     plt.show
 
 
 #twist distribution
-def twist_distribution_graph():
+def plot_twist_distribution():
     spanpos = []
     twist_dist = []
 
@@ -33,11 +33,12 @@ def twist_distribution_graph():
 
     while y_position <= b/2:
         spanpos.append(y_position)
-        twist_dist.append(twist(y_position)*180/math.pi)
-        y_position += 0.5
+        twist_dist.append(twist_function(y_position)*180/math.pi)
+        y_position += 0.01
 
     plt.plot(spanpos, twist_dist)
     plt.xlabel("Spanwise position [m]")
     plt.ylabel("Angle of twist [degree]")
     plt.show
 
+plot_twist_distribution()
