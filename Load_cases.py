1f7362fa52a8056ae85a589fad4a30e331912a35
Ignacy Bachanski <ignacy.b@icloud.com>
2025-12-02 16:29:12 +0100

    Changed load case

--- Load_cases.py
+++ Load_cases.py
@@ -1,9 +1,9 @@
 #Put here cases from V-n diaagrams
 
 #============CASE I============#
-mass_aircraft = 3.2 * 5794 # Mass of the aircraft in kg
-v_cruise =  100 # Flight speed in m/s
-rho_cruise = 1.225 # Air density at cruise altitude in kg/m^3
+mass_aircraft = 3.1 * 6804  # Mass of the aircraft in kg
+v_cruise =  97.2 # Flight speed in m/s
+rho_cruise = 0.28741 # Air density at cruise altitude in kg/m^3
 mass_fuel = 0 # Total fuel mass in kg
 
 #============CASE II============#
