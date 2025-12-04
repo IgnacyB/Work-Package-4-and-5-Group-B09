5cb0f8877e8cdca6aa3620f6dbadd5365e3197ab
lisanne-b <l.m.l.boer@student.tudelft.nl>
2025-12-04 16:28:54 +0100

    updated graph code

--- Deflection_Graphs.py
+++ Deflection_Graphs.py
@@ -16,30 +16,77 @@ def plot_lateral_deflection(title=None):
         plt.title(f"{title} — Lateral deflection")
     else:
         plt.title("Lateral deflection along wingspan")
+
     plt.xlabel("Spanwise position [m]")
     plt.ylabel("Lateral deflection [m]")
+
+    plt.plot(y_grid[-1], v_grid[-1], marker='o', color='blue')
+
+    plt.annotate(
+        f'{v_grid[-1]:.2f}',
+        (y_grid[-1], v_grid[-1]),
+        xytext=(-10, -20),
+        textcoords="offset points",
+        ha="left",
+        va="bottom")
+
     plt.grid(True)
     plt.show()
 
 
-# twist distribution
 def plot_twist_distribution(title=None):
     """Plot twist distribution. If title provided, include it in the figure title."""
     y_grid, twist_grid = twist_function()
 
-    plt.plot(y_grid, twist_grid * 180 / math.pi, lw=2, color="tab:blue")
+    # Convert radians → degrees
+    twist_deg = twist_grid * 180 / math.pi
+
+    # Plot twist distribution
+    plt.plot(y_grid, twist_deg, lw=2, color="tab:blue")
+    if twist_deg[-1] < 0:
+        plt.gca().invert_yaxis()
+
     if title:
         plt.title(f"{title} — Twist distribution")
     else:
-        plt.xlabel("Spanwise position [m]")
         plt.title("Twist distribution along wingspan")
+
     plt.xlabel("Spanwise position [m]")
     plt.ylabel("Angle of twist [degree]")
+
+    plt.plot(y_grid[-1], twist_deg[-1], marker='o', color='blue')
+
+    plt.annotate(
+        f'{twist_deg[-1]:.2f}',
+        (y_grid[-1], twist_deg[-1]),
+        xytext=(-10, -20),
+        textcoords="offset points",
+        ha="left",
+        va="bottom")
+
     plt.grid(True)
     plt.show()
 
 
 
-if __name__ == "__main__":
-    plot_lateral_deflection()
-    plot_twist_distribution()
+# # twist distribution
+# def plot_twist_distribution(title=None):
+#     """Plot twist distribution. If title provided, include it in the figure title."""
+#     y_grid, twist_grid = twist_function()
+
+#     plt.plot(y_grid, twist_grid * 180 / math.pi, lw=2, color="tab:blue")
+#     if title:
+#         plt.title(f"{title} — Twist distribution")
+#     else:
+#         plt.xlabel("Spanwise position [m]")
+#         plt.title("Twist distribution along wingspan")
+#     plt.xlabel("Spanwise position [m]")
+#     plt.ylabel("Angle of twist [degree]")
+#     plt.grid(True)
+#     plt.show()
+
+
+
+# if __name__ == "__main__":
+#     plot_lateral_deflection()
+#     plot_twist_distribution()
