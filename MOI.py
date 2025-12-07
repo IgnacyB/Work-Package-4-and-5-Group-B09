809dedebca5032c77b3d26e54a3d44c36ba5730b
Ignacy Bachanski <ignacy.b@icloud.com>
2025-12-07 17:05:58 +0100

    Minor fix

--- MOI.py
+++ MOI.py
@@ -282,10 +282,10 @@ def plot_MOI_multi_cell(y=None, n=200, figsize=(10,4), dpi=100):
     plt.show()
 
 #Setting MOI grid for further calculations
-from grid_setup import y_grid
+from grid_setup import y_arr
 MOI_vec = np.vectorize(MOI)
-MOI_grid = MOI_vec(y_grid)[0]  # extract only MOI values, not centroids
-cx_grid = MOI_vec(y_grid)[1]  # extract only centroid x locations
+MOI_grid = MOI_vec(y_arr)[0]  # extract only MOI values, not centroids
+cx_grid = MOI_vec(y_arr)[1]  # extract only centroid x locations
 
 if __name__ == "__main__":
     plot_MOI_single_cell()
