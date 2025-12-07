5b9e96b897b8baa0b23c517ff4b5c372dbcd962d
lisanne-b <l.m.l.boer@student.tudelft.nl>
2025-12-07 16:32:45 +0100

    integrated thickness_distribution in MOI file

--- MOI.py
+++ MOI.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy as sp
 from Aircraft_parameters import b
 import matplotlib.pyplot as plt
+from thickness_distribution import t_front_func, t_middle_func, t_rear_func
 
 """
 The main MOI function is a logic function. If check for the different cases of the design what equation to use.
@@ -41,9 +42,12 @@ Therefore more in depth working of the code is only explained in single cell, mu
 def MOI_single_cell(y):
     #import the necessary parameters form other files x
     from airfoil_geometry import t_skin as skin_thickness
-    from airfoil_geometry import t_middle as thickness_middle
-    from airfoil_geometry import t_front as thickness_front
-    from airfoil_geometry import t_rear as thickness_rear
+    from thickness_distribution import t_middle_func
+    thickness_middle = t_middle_func(y)
+    from thickness_distribution import t_front_func
+    thickness_front = t_front_func(y)
+    from thickness_distribution import t_rear_func 
+    thickness_rear = t_rear_func(y)
     from airfoil_geometry import a_stringer as mass_stringer
     from airfoil_geometry import n_stringer
     from airfoil_geometry import location_front as chord_position_front
@@ -119,9 +123,12 @@ def MOI_single_cell(y):
 def MOI_multi_cell(y):
     #import the necessary functions from other
     from airfoil_geometry import t_skin as skin_thickness
-    from airfoil_geometry import t_middle as thickness_middle
-    from airfoil_geometry import t_front as thickness_front
-    from airfoil_geometry import t_rear as thickness_rear
+    from thickness_distribution import t_middle_func
+    thickness_middle = t_middle_func(y)
+    from thickness_distribution import t_front_func
+    thickness_front = t_front_func(y)
+    from thickness_distribution import t_rear_func 
+    thickness_rear = t_rear_func(y)
     from airfoil_geometry import a_stringer as mass_stringer
     from airfoil_geometry import n_stringer
     from airfoil_geometry import location_front as chord_position_front
