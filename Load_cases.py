bf47a08d7a29334c59d0753afc0e2160aad9db01
Jan Horawa <jan.horawa@gmail.com>
2025-12-09 16:13:41 +0100

    cuh

--- Load_cases.py
+++ Load_cases.py
@@ -1,10 +1,10 @@
 #Put here cases from V-n diaagrams
 
 #============CASE I============#
-mass_aircraft = 3.1 * 6804  # Mass of the aircraft in kg
-v_cruise =  97.2 # Flight speed in m/s
-rho_cruise = 0.28741 # Air density at cruise altitude in kg/m^3
-mass_fuel = 0 # Total fuel mass in kg
+mass_aircraft = 2.9 * 9425 # Mass of the aircraft in kg
+v_cruise =  121.5 # Flight speed in m/s
+rho_cruise = 1.225 # Air density at cruise altitude in kg/m^3
+mass_fuel = 2622 # Total fuel mass in kg
 
 #============CASE II============#
 
